b4373b507e1350ced040eb4f4afa4db17fb7e4bd
Manav Misra <manavm@visionwebsoft.com>
2023-01-11 12:27:12 -0600

    test: getStudentById

--- app/lib.test.js
+++ app/lib.test.js
@@ -1,11 +1,22 @@
 import { it, expect } from "vitest";
-import { getAllNamesFromRoster } from "./lib.js";
+import { getAllNamesFromRoster, getStudentById } from "./lib.js";
 
 it("should return all names from roster", () => {
   const input = [{ name: "Jack" }, { name: "Jill" }, { name: "John" }];
   const expected = ["Jack", "Jill", "John"];
 
   const actual = getAllNamesFromRoster(input);
+
+  expect(actual).toEqual(expected);
+});
+
+it("should return student by id", () => {
+  const rosterInput = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+  const id2FindInput = 3;
+
+  const expected = { id: 3 };
+
+  const actual = getStudentById(rosterInput, id2FindInput);
 
   expect(actual).toEqual(expected);
 });
