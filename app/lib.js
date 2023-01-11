14fd0bb3a7bbad7ba606ff0326516249e5601aa7
Manav Misra <manavm@visionwebsoft.com>
2023-01-11 12:24:26 -0600

    feat: getStudentById

--- app/lib.js
+++ app/lib.js
@@ -1,2 +1,5 @@
 export const getAllNamesFromRoster = (roster) =>
   roster.map((person) => person.name);
+
+export const getStudentById = (roster, id2Find) =>
+  roster.find((person) => person.id === id2Find);
