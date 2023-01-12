794f0718e2b1e6ce85b631b48fcfeb7b8a9d7d8e
Manav Misra <manavm@visionwebsoft.com>
2023-01-12 11:50:18 -0600

    feat: :sparkles: add requirements from README
    TODO: Write tests for these.

--- app/lib.js
+++ app/lib.js
@@ -38,3 +38,56 @@ export const addGrade = ({ roster, gradeType, score, studentId }) => {
   };
 };
 
+export const updateGrade = ({ roster, gradeId, updatedScore, studentId }) => {
+  const foundStudent = getStudentById(roster, studentId);
+
+  const updatedGrades = foundStudent.grades.map((grade) => {
+    // If it's the grade we're looking for, update the score
+    if (grade.id === gradeId) {
+      return {
+        ...grade,
+        score: updatedScore,
+      };
+    }
+
+    // Otherwise, return the grade as is
+    return grade;
+  });
+
+  // Mutation is okay here because we're not mutating the original object
+  // It is contained within the function body exclusively
+  // 'foundStudent' is a LOCALLY SCOPED VARIABLE that is only available within this function
+  foundStudent.grades = [...updatedGrades];
+
+  const updatedRoster = roster.map((student) => {
+    if (student.id === studentId) {
+      return foundStudent;
+    }
+
+    return student;
+  });
+
+  return updatedRoster;
+};
+
+export const deleteGrade = ({ roster, gradeId, studentId }) => {
+  const foundStudent = getStudentById(roster, studentId);
+
+  // Filter out the grade we want to delete
+  const gradesWithoutDeletedGrade = foundStudent.grades.filter(
+    // If the grade's id is NOT the gradeId we're looking for, keep it
+    (grade) => grade.id !== gradeId
+  );
+
+  foundStudent.grades = [...gradesWithoutDeletedGrade];
+
+  const updatedRoster = roster.map((student) => {
+    if (student.id === studentId) {
+      return foundStudent;
+    }
+
+    return student;
+  });
+
+  return updatedRoster;
+};
