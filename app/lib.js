0a68aa48036508e9fc98497bc5dc0d1706b1e3c6
Manav Misra <manavm@visionwebsoft.com>
2023-01-12 10:29:55 -0600

    feat: :sparkles: getStudentGradeById

--- app/lib.js
+++ app/lib.js
@@ -3,3 +3,10 @@ export const getAllNamesFromRoster = (roster) =>
 
 export const getStudentById = (roster, id2Find) =>
   roster.find((person) => person.id === id2Find);
+
+export const getStudentGradeById = ({ roster, gradeId, studentId }) => {
+  const foundStudent = getStudentById(roster, studentId);
+  const foundGrade = foundStudent.grades.find((grade) => grade.id === gradeId);
+
+  return { ...foundGrade, name: foundStudent.name };
+};
--- app/lib.test.js
+++ app/lib.test.js
@@ -1,5 +1,9 @@
-import { it, expect } from "vitest";
-import { getAllNamesFromRoster, getStudentById } from "./lib.js";
+import { expect, it } from "vitest";
+import {
+  getAllNamesFromRoster,
+  getStudentById,
+  getStudentGradeById,
+} from "./lib.js";
 
 it("should return all names from roster", () => {
   const input = [{ name: "Jack" }, { name: "Jill" }, { name: "John" }];
@@ -17,6 +21,32 @@ it("should return student by id", () => {
   const expected = { id: 3 };
 
   const actual = getStudentById(rosterInput, id2FindInput);
+
+  expect(actual).toEqual(expected);
+});
+
+it("should find a specific grade for a specific student", () => {
+  const inputRoster = [
+    {
+      id: 1,
+      name: "Jack",
+      grades: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    },
+    {
+      id: 2,
+      name: "Jill",
+      grades: [{ id: 1 }, { id: 2 }, { id: 3 }],
+    },
+  ];
+  const inputGradeId = 2;
+  const inputStudentId = 1;
+  const expected = { id: 2, name: "Jack" };
+
+  const actual = getStudentGradeById({
+    roster: inputRoster,
+    gradeId: inputGradeId,
+    studentId: inputStudentId,
+  });
 
   expect(actual).toEqual(expected);
 });
